5029c0a9c2ba6202edac8e81b975ebf543cee99e
Vincent Miller <vrwmiller@gmail.com>
2020-12-29 20:16:33 +0000

    Check in website

---
+++ js/stations.js
@@ -0,0 +1,5 @@
+
+var office = document.createElement("office");
+office.setAttribute("type", "text");
+
+endpoint = 'https://api.weather.gov/offices/' + office;
---
+++ js/test.js
@@ -0,0 +1,7 @@
+
+var a = 0;
+
+while (a < 10) {
+  document.write("a = " + a + "<br>");
+  a++;
+}
